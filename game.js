5842861b05a22913e32db7a8fcff0f345d83776c
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 10:51:49 +0200

    try sound

--- game.js
+++ game.js
@@ -15,6 +15,10 @@ $("btn").on("mousedown", function(){
     console.log(this);
     nextSequence();
     $(this).animate({opacity: 0.25},1000);
-    mySounds.src = "./sounds/" + randomChosenColour + ".mp3";   // For now plays the correct color, not pressed color 
-    mySounds.play();
+    for (i = 0; i < buttonColours.length;i++) {
+        if ($(this).hasClass(randomChosenColour) === buttonColours[i]) {
+            mySounds.src = "./sounds/" + randomChosenColour + ".mp3";
+            mySounds.play();
+        }
+    }
 });
