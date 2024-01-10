6fa1588d16babd47079051b4152dbeacffda8a42
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 11:12:07 +0200

    fixed class selector as .btn

--- game.js
+++ game.js
@@ -11,13 +11,16 @@ function nextSequence(){
     console.log(randomChosenColour);
 }
 
-$("btn").on("mousedown", function(){
+$(".btn").on("mousedown", function(){
     console.log(this);
     nextSequence();
     $(this).animate({opacity: 0.25},1000);
+});
+
+
     //for (i = 0; i < buttonColours.length;i++) {
     //mySounds.src = "./sounds/" + randomChosenColour + ".mp3";
     //mySounds.play();
     //}
     //if ($(this).hasClass(randomChosenColour) === buttonColours[i]) {
-});
+
