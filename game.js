6172fcab633a158d4188bf8f58e90422ff25855a
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 11:33:45 +0200

    try to get id from .id

--- game.js
+++ game.js
@@ -15,6 +15,9 @@ $(".btn").on("mousedown", function(){
     console.log(this);
     nextSequence();
     $(this).animate({opacity: 0.25},500, function(){$(this).delay(500).animate({opacity: 1}, 500);
+    console.log(this.id);
+    var userChosenColour = this.id; // does this work?
+    console.log(userChosenColour);
     });
 
 
