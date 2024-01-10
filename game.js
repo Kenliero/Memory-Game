e87558a8eda6ef9bc15ed81a1bb011f8fe624f40
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 09:28:07 +0200

    use THIS when calling event

--- game.js
+++ game.js
@@ -6,14 +6,14 @@ var mySounds = new Audio();
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push[randomChosenColour];
+    gamePattern.push(randomChosenColour);
     console.log(randomChosenColour);
 }
 
 $("button").on("mousedown", function(event){
     console.log(event);
     nextSequence();
-    //$("button").hasClass("randomChosenColour").animate({opacity: 0.25},1000);
+    $(this).animate({opacity: 0.25},1000);
 });
 
 
