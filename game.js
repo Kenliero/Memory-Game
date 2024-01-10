b2893e6a79bb04eb6361327bd0f6d78c101863bc
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 10:53:38 +0200

    correct color played

--- game.js
+++ game.js
@@ -15,10 +15,9 @@ $("btn").on("mousedown", function(){
     console.log(this);
     nextSequence();
     $(this).animate({opacity: 0.25},1000);
-    for (i = 0; i < buttonColours.length;i++) {
-        if ($(this).hasClass(randomChosenColour) === buttonColours[i]) {
-            mySounds.src = "./sounds/" + randomChosenColour + ".mp3";
-            mySounds.play();
-        }
-    }
+    //for (i = 0; i < buttonColours.length;i++) {
+    mySounds.src = "./sounds/" + randomChosenColour + ".mp3";
+    mySounds.play();
+    //}
+    //if ($(this).hasClass(randomChosenColour) === buttonColours[i]) {
 });
