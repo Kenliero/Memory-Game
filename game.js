abfd90dd4b29836230e70e4e9b1de15a822d10bd
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-10 07:40:51 +0200

    log event

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour = "";
 var gamePattern= [];
+var mySounds = new Audio();
+
+$("button").on("mousedown", function(event){
+    console.log(event);
+    // mySounds.play($(event.click));
+})
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
     gamePattern.push[randomChosenColour];
 }
+
+nextSequence();
+$("button").hasClass("randomChosenColour").animate({backgroundColor: "#000000"},1000);
+
+// .animate({backgroundColor: "#ff0000"}, 1000);
